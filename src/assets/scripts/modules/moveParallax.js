7e5552b5ac752e563086aa3098e9294159559725
AntonVoronezh <pstut@mail.ru>
2019-01-04 16:31:56 +0300

    moveLayers

--- src/assets/scripts/modules/moveParallax.js
+++ src/assets/scripts/modules/moveParallax.js
@@ -2,5 +2,8 @@ export default function moveParallax() {
     const parallaxContainer = document.querySelector(".parallax");
     const layers = Array.from(parallaxContainer.children);
   
+    const moveLayers = e => {
+  
+    };
   
   }
